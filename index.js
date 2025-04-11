5128311d4b171996ddd58cfc2c63bd6a8fd412ef
Aitor Cunill <aitorcunill@gmail.com>
2025-04-11 10:31:46 -0300

    CD/CI Test 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Estrella Platinada, El Mundo!!!!</h1>')
+  res.send('<h1>Prueba Expectacular</h1>')
 })
 
 const PORT = 8080
