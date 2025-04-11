cc77216727a95b5cba043ab2def9582eed1e0457
Aitor Cunill <aitorcunill@gmail.com>
2025-04-11 11:12:51 -0300

    CD/CI Test 4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello wolrd 2.0!</h1>')
+  res.send('<h1>Hello Render!</h1>')
 })
 
 const PORT = 8080
