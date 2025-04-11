8953cc6d176140571e26cba8997c52d0fcd376ba
Aitor Cunill <aitorcunill@gmail.com>
2025-04-11 11:42:02 -0300

    Render deploy test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Render!</h1>')
+  res.send('<h1>Hello Render! This is a deployment test!!</h1>')
 })
 
 const PORT = 8080
